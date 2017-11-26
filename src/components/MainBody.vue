9785bd90b7e7d36ee9a74f3a4134182b147dcf76
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-11-26 20:19:53 +0100

    code generation optimization

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -18,15 +18,17 @@
             <toggle-button class="button" :color="{checked: '#2BA027', unchecked: '#B9B6B6'}" v-model="isClickable" :sync="true" :disabled="true" :labels="{checked: 'clickable link', unchecked: 'no link'}" :width="100"/>
             <input :size="30" v-model="customLink" class="text" placeholder="insert link (e.g. discord invite link)"/>
           </div>
-          <div v-if="isClickable">
-            <a v-if="isSquare" :href="customLink">
+          <div ref="preview" class="preview">
+            <div v-if="isClickable">
+              <a v-if="isSquare" :href="customLink">
+                <DiscordSwirl class="previewbox" v-if="isSquare" :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
+              </a>
+              <DiscordCorner :customLink="customLink" :style="getStyle" class="previewbox" v-else :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
+            </div>
+            <div v-else>
               <DiscordSwirl class="previewbox" v-if="isSquare" :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
-            </a>
-            <DiscordCorner :customLink="customLink" :style="getStyle" class="previewbox verticalFlip" v-else :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
-          </div>
-          <div v-else>
-            <DiscordSwirl class="previewbox" v-if="isSquare" :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
-            <DiscordCorner :style="getStyle" class="previewbox verticalFlip" v-else :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
+              <DiscordCorner :style="getStyle" class="previewbox" v-else :width="size" :height="size" :discordfill="colors.discordfill.hex" :discordcolor="colors.discordcolor.hex" />
+            </div>
           </div>
         </div>
       </div>
@@ -34,7 +36,7 @@
         <chrome-picker v-model="colors.discordcolor"/>
         <chrome-picker v-model="colors.discordfill"/>
       </div>
-      <textarea v-model="getCode"/>
+      <textarea v-model="previewCode"/>
     </section>
     <footer>
       <p>This project is neither build nor endorsed by Discord.</p>
@@ -58,6 +60,7 @@ export default {
   },
   data () {
     return {
+      previewCode: "",
       size: 100,
       isSquare: false,
       isLeft: true,
@@ -118,6 +121,17 @@ export default {
     changeColor: function (discordcolor, discordfill) {
       this.colors.discordcolor = {hex: discordcolor, a: 1}
       this.colors.discordfill = {hex: discordfill, a: 1}
+    },
+    updatePreviewCode:  function() {
+      this.$nextTick(function () {
+        // => 'DOM loaded and ready'
+        if (this.$refs.preview == null) {
+          this.previewCode = ""
+        } else {
+          this.previewCode = this.$refs.preview.innerHTML
+          this.previewCode += "\n\n<style type='text/css'>.rotatethis { transform: rotate(-45deg); transform-origin: 60px 60px; } .discord-logo { transform: scale(0.7); transform-origin: 24px 24px; } .discord-logo .discord-outer-layer { transition: transform 800ms cubic-bezier(0.7, 1, 0.7, 1); transform-origin: 50% 50%; } .discord-logo-container:hover .discord-outer-layer { transform: scale(1.5) rotate(360deg); } .discord-logo .discord-middle-layer { transition: transform 800ms cubic-bezier(0.5, 1, 0.5, 1); transform-origin: 50% 50%; } .discord-logo-container:hover .discord-middle-layer { transform: scale(1.4) rotate(360deg); } .discord-logo .discord-inner-layer { transition: transform 800ms cubic-bezier(0.3, 1, 0.3, 1); transform-origin: 50% 50%; } .discord-logo-container:hover .discord-inner-layer { transform: scale(1.3) rotate(360deg); } .discord-logo .discord-original { transition: visibility 0ms; transition-delay: 800ms; } .discord-logo-container:hover .discord-original { visibility: hidden; transition-delay: 0ms; }</style>"
+        }
+      })
     }
   },
   computed: {
@@ -154,42 +168,47 @@ export default {
       get: function() {
         return this.customLink == "" ? false : true
       }
-    },
-    getCode: {
-      get: function() {
-        var actualSize = this.isSquare ? this.size : "120"
-        var discordHTML
-        var discordSquareHTML
-        var discordStyle = "<style type='text/css'> .discord-corner{ " + this.rotation + " } "
-        discordStyle += ".discord-logo { transform: scale(0.7); transform-origin: 24px 24px; } .discord-logo .discord-outer-layer { transition: transform 800ms cubic-bezier(0.7, 1, 0.7, 1); transform-origin: 50% 50%; } .discord-logo-container:hover .discord-outer-layer { transform: scale(1.5) rotate(360deg); } .discord-logo .discord-middle-layer { transition: transform 800ms cubic-bezier(0.5, 1, 0.5, 1); transform-origin: 50% 50%; } .discord-logo-container:hover .discord-middle-layer { transform: scale(1.4) rotate(360deg); } .discord-logo .discord-inner-layer { transition: transform 800ms cubic-bezier(0.3, 1, 0.3, 1); transform-origin: 50% 50%; } .discord-logo-container:hover .discord-inner-layer { transform: scale(1.3) rotate(360deg); } .discord-logo .discord-original { transition: visibility 0ms; transition-delay: 800ms; } .discord-logo-container:hover .discord-original { visibility: hidden; transition-delay: 0ms; } "
-        discordStyle += this.isSquare ? "" : ".rotatethis { transform: rotate(-45deg); transform-origin: 60px 60px; }"
-
-        discordStyle += "</style>"
-
-        discordSquareHTML = "<svg color='" + this.colors.discordcolor.hex +"' fill='" + this.colors.discordfill.hex + "' width='" + actualSize + "' height='" + actualSize + "' xmlns:xlink='http://www.w3.org/1999/xlink' class='discord-logo-container' viewBox='0 0 48 48'> <rect width='100%' height='100%' fill='currentfill' /> <defs> <g> <path id='discord-logo' fill='currentcolor' d='m40,12c0,0 -4.585,-3.588 -10,-4l-0.488,0.976c4.896,1.198 7.142,2.915 9.488,5.024c-4.045,-2.065 -8.039,-4 -15,-4s-10.955,1.935 -15,4c2.346,-2.109 5.018,-4.015 9.488,-5.024l-0.488,-0.976c-5.681,0.537 -10,4 -10,4s-5.121,7.425 -6,22c5.162,5.953 13,6 13,6l1.639,-2.185c-2.782,-0.967 -5.924,-2.694 -8.639,-5.815c3.238,2.45 8.125,5 16,5s12.762,-2.55 16,-5c-2.715,3.121 -5.857,4.848 -8.639,5.815l1.639,2.185c0,0 7.838,-0.047 13,-6c-0.879,-14.575 -6,-22 -6,-22zm-22.5,18c-1.933,0 -3.5,-1.791 -3.5,-4c0,-2.209 1.567,-4 3.5,-4s3.5,1.791 3.5,4c0,2.209 -1.567,4 -3.5,4zm13,0c-1.933,0 -3.5,-1.791 -3.5,-4c0,-2.209 1.567,-4 3.5,-4s3.5,1.791 3.5,4c0,2.209 -1.567,4 -3.5,4z' /> <mask id='mask-outer-layer'> <rect width='100%' height='100%' fill='#FFFFFF' /> <circle r='42%' cx='50%' cy='50%' fill='#000000' /> </mask> <mask id='mask-middle-layer'> <rect width='100%' height='100%' fill='#000000' /> <circle r='43%' cx='50%' cy='50%' fill='#FFFFFF' /> <circle r='32%' cx='50%' cy='50%' fill='#000000' /> </mask> <mask id='mask-inner-layer'> <rect width='100%' height='100%' fill='#000000' /> <circle r='32%' cx='50%' cy='50%' fill='#FFFFFF' /> </mask> </g> </defs> <g class='discord-logo'> <use class='discord-original' href='#discord-logo' /> <use class='discord-outer-layer' href='#discord-logo' mask='url(#mask-outer-layer)' /> <use class='discord-middle-layer' href='#discord-logo' mask='url(#mask-middle-layer)' /> <use class='discord-inner-layer' href='#discord-logo' mask='url(#mask-inner-layer)' /> </g> </svg>"
-        if (this.isClickable) {
-          discordSquareHTML = "<a href='" + this.customLink + "'>" + discordSquareHTML + "</a>"
-        }
-        if (this.isSquare) {
-          discordHTML = discordSquareHTML
-        } else {
-          discordHTML = "<svg class='discord-corner' color='" + this.colors.discordcolor.hex +"' fill='" + this.colors.discordfill.hex +"' width='" + this.size + "' height='" + this.size + "' viewBox='0 0 250 250'><g>"
-          discordHTML += this.isClickable ? "<a href='" + this.customLink + "'>" : ""
-          discordHTML += "<path d='M0,0 L0,250 L250,0 Z' fill='" + this.colors.discordfill.hex +"' />"
-          discordHTML += this.isClickable ? "</a>" : ""
-          discordHTML += "</g><g class='rotatethis'>"
-          discordHTML += discordSquareHTML
-          discordHTML += "</g></svg>"
-        }
-        return discordHTML + "\n\n\n\n" + discordStyle
-      }
     }
-  }
+  },
+  watch: {
+    size: function () {
+      this.updatePreviewCode();
+    },
+    isSquare: function () {
+      this.updatePreviewCode();
+    },
+    isLeft: function () {
+      this.updatePreviewCode();
+    },
+    isTop: function () {
+      this.updatePreviewCode();
+    },
+    isAbsolutePosition: function () {
+      this.updatePreviewCode();
+    },
+    colors: {
+      handler: function () {
+        this.updatePreviewCode();
+      },
+      deep: true
+    },
+    customLink: function () {
+      this.updatePreviewCode();
+    }
+  },
+    created: function() {
+      this.updatePreviewCode()
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.debug {
+  height: 500px;
+}
+
+
 header {
   padding-right: 20px;
   padding-left: 20px;
