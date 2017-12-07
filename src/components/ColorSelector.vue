b0e4f93d2fe5d18687f94fe3b27680bf40b698a1
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-12-07 13:34:54 +0100

    added rainbow option to color selector

--- src/components/ColorSelector.vue
+++ src/components/ColorSelector.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="buttons">
-    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#7289DA','#FFFFFF')" :width="size" :height="size" discordfill="#7289DA" discordcolor="#FFFFFF"/>
-    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#7289DA','#2C2F33')" :width="size" :height="size" discordfill="#7289DA" discordcolor="#2C2F33"/><br/>
-    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#FFFFFF','#7289DA')" :width="size" :height="size" discordfill="#FFFFFF" discordcolor="#7289DA"/>
-    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#FFFFFF','#2C2F33')" :width="size" :height="size" discordfill="#FFFFFF" discordcolor="#2C2F33"/><br/>
-    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#2C2F33','#7289DA')" :width="size" :height="size" discordfill="#2C2F33" discordcolor="#7289DA"/>
-    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#2C2F33','#FFFFFF')" :width="size" :height="size" discordfill="#2C2F33" discordcolor="#FFFFFF"/>
+    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#7289DA','#FFFFFF') + setRainbow(false)" :width="size" :height="size" discordfill="#7289DA" discordcolor="#FFFFFF"/>
+    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#7289DA','#2C2F33') + setRainbow(false)" :width="size" :height="size" discordfill="#7289DA" discordcolor="#2C2F33"/><br/>
+    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#FFFFFF','#7289DA') + setRainbow(false)" :width="size" :height="size" discordfill="#FFFFFF" discordcolor="#7289DA"/>
+    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#FFFFFF','#2C2F33') + setRainbow(false)" :width="size" :height="size" discordfill="#FFFFFF" discordcolor="#2C2F33"/><br/>
+    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#2C2F33','#7289DA') + setRainbow(false)" :width="size" :height="size" discordfill="#2C2F33" discordcolor="#7289DA"/>
+    <DiscordLogo :animationStyle="animationStyle" :customLink="customLink" @click.native="setColor('#2C2F33','#FFFFFF') + setRainbow(false)" :width="size" :height="size" discordfill="#2C2F33" discordcolor="#FFFFFF"/><br />
+    <DiscordLogo  animationStyle="none"           :customLink="customLink" @click.native="setRainbow(true)"    :isRainbow="isRainbow"       :width="size" :height="size" :discordfill="colors.discordfill"/>
   </div>
 </template>
 
@@ -34,6 +35,10 @@ export default {
     animationStyle: {
       type: String,
       default: 'swirl'
+    },
+    isRainbow: {
+      type: Boolean,
+      default: true
     }
   },
 	methods: {
@@ -41,6 +46,9 @@ export default {
       this.colors.discordcolor = textColor,
       this.colors.discordfill = backgroundColor
       this.$emit('colorChange', this.colors);
+		},
+    setRainbow: function (value) {
+      this.$emit('rainbowChange', value);
 		}
   }
 }
