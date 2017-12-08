0fbbe7f27ce722863cc1b68e218ea9e401a9db7c
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-12-08 12:30:17 +0100

    angry eyes and wink added
    UI for selecting angry eyes or wink needs to be added

--- src/components/DiscordLogo.vue
+++ src/components/DiscordLogo.vue
@@ -4,15 +4,33 @@
     <defs>
       <g>
         <defs>
-          <path id="discord-logo-face" fill="currentcolor" d="M40,12C40,12,35.415,8.412,30,8L29.512,8.976C34.408,10.174,36.654,11.891,39,14C34.955,11.935,30.961,10,24,10S13.045,11.935,9,14C11.346,11.891,14.018,9.985,18.488,8.976L18,8C12.319,8.537,8,12,8,12S2.879,19.425,2,34C7.162,39.953,15,40,15,40L16.639,37.815C13.857,36.848,10.715,35.121,8,32C11.238,34.45,16.125,37,24,37S36.762,34.45,40,32C37.285,35.121,34.143,36.848,31.361,37.815L33,40C33,40,40.838,39.953,46,34C45.121,19.425,40,12,40,12Z" />
-          <path id="discord-logo-face-left-eye" fill="currentfill" d="M17.5,30C15.567,30,14,28.209,14,26C14,23.791,15.567,22,17.5,22S21,23.791,21,26C21,28.209,19.433,30,17.5,30Z" />
-          <path id="discord-logo-face-right-eye" fill="currentfill" d="M30.5,30C28.567,30,27,28.209,27,26C27,23.791,28.567,22,30.5,22S34,23.791,34,26C34,28.209,32.433,30,30.5,30Z" />
+          <path id="discord-def-face" fill="currentcolor" d="M40,12C40,12,35.415,8.412,30,8L29.512,8.976C34.408,10.174,36.654,11.891,39,14C34.955,11.935,30.961,10,24,10S13.045,11.935,9,14C11.346,11.891,14.018,9.985,18.488,8.976L18,8C12.319,8.537,8,12,8,12S2.879,19.425,2,34C7.162,39.953,15,40,15,40L16.639,37.815C13.857,36.848,10.715,35.121,8,32C11.238,34.45,16.125,37,24,37S36.762,34.45,40,32C37.285,35.121,34.143,36.848,31.361,37.815L33,40C33,40,40.838,39.953,46,34C45.121,19.425,40,12,40,12Z" />
+          <g id="discord-def-face-eyes">
+            <path id="discord-def-face-left-eye" fill="currentfill" d="M17.5,30C15.567,30,14,28.209,14,26C14,23.791,15.567,22,17.5,22S21,23.791,21,26C21,28.209,19.433,30,17.5,30Z" />
+            <path id="discord-def-face-right-eye" fill="currentfill" d="M30.5,30C28.567,30,27,28.209,27,26C27,23.791,28.567,22,30.5,22S34,23.791,34,26C34,28.209,32.433,30,30.5,30Z" />
+          </g>
         </defs>
         <g id="discord-logo">
-          <use href="#discord-logo-face" />
-          <g id="discord-eyes">
-            <use href="#discord-logo-face-left-eye" />
-            <use href="#discord-logo-face-right-eye" />
+          <use href="#discord-def-face" />
+          <g id="discord-logo-eyes">
+            <mask id="mask-right-eye-wink">
+              <ellipse fill="#FFFFFF" ry="15" rx="15" cy="39.7" cx="35" />
+              <ellipse fill="#000000" ry="15" rx="15" cy="40.5" cx="34" />
+            </mask>
+            <mask id="mask-eyes-angry">
+              <rect height="48" width="48" y="0" x="0" fill="#FFFFFF"/>
+              <rect transform="rotate(45 24,14.5)" height="24" width="24" y="2.5" x="12" fill="#000000"/>
+            </mask>
+            <g v-if="discordEyes == 'wink'">
+              <use href="#discord-def-face-left-eye" />
+              <use href="#discord-def-face-right-eye" mask="url(#mask-right-eye-wink)"/>
+            </g>
+            <g v-else-if="discordEyes == 'angry'">
+              <use href="#discord-def-face-eyes" mask="url(#mask-eyes-angry)"/>
+            </g>
+            <g v-else>
+              <use href="#discord-def-face-eyes"/>
+            </g>
           </g>
         </g>
         <mask id="mask-outer-layer">
@@ -93,6 +111,10 @@ export default {
     isRainbow: {
       type: Boolean,
       default: false
+    },
+    discordEyes: {
+      type: String,
+      default: 'wink' //none wink angry
     }
 	}
 }
