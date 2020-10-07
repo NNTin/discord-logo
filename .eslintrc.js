57b4c19d77cb29651d4766feda8305cd1988b3d4
zurda <m.weizman@gmail.com>
2020-10-07 19:32:41 +0100

    Add linter rules to temporarily remove issues
    This update adds rules to the linter that would remove all existing errors and warnings. This way, we can set up CI with no errors, then later remove these rules and fix these issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,17 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     'plugin:vue/recommended'
   ],
-  rules: {}
+  rules: {
+    'vue/require-explicit-emits': 0,
+    'vue/custom-event-name-casing': 0,
+    'vue/html-indent': 0,
+    'vue/no-deprecated-v-on-native-modifier': 0,
+    'vue/no-side-effects-in-computed-properties': 0,
+    'vue/attributes-order': 0,
+    'vue/max-attributes-per-line': 0,
+    'no-redeclare': 0,
+    'no-empty': 0,
+    'vue/this-in-template': 0,
+    'no-mixed-spaces-and-tabs': 0
+  }
 }
